7d0efdbea13d55cd53a4d5747da384a139de1d23
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-15 10:52:19 -0700

    add list create blog posts method class

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerialzer
+# to create a crud CREATE
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerialzer
+    
