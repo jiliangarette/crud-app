2b39d78808e0a9a330163430e022959861b8bfa2
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-15 10:50:33 -0700

    add models to generate table blog post in database

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# N O T E S
+
+#   django use ORM (object relational mapping)
+#   it maps python project to a dabase instance
+
+
+# to create a database table
+class BlogPost(models.Model):
+    # to create a column
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    # to see a data when a model is created
+    def __str__(self):
+        return self.title
