c553a12acab0593f3b94655bb93985e9dfd3cf20
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-16 08:21:48 -0700

    complete the crud views along with mock custom api for practice

--- server/api/views.py
+++ server/api/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import BlogPost
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import BlogPostSerialzer
+from .models import BlogPost
+from rest_framework.views import APIView
+
 # to create a crud CREATE
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerialzer
     
+    # to overide an view class
+    def delete(self, request, *args,**kwargs):
+        # to delete all
+        BlogPost.objects.all().delete() 
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerialzer
+    lookup_field = "pk"
+    
+class Bloglists(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get("title", "")
+        
+        if title:
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+        
+        serializer = BlogPostSerialzer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
