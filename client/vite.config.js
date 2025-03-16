09dacad52ef22975b95d76b720bb1b7f2b39c61c
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-16 09:17:21 -0700

    chore(setup): initialize client setup with routing, styles, and dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+      "@components": path.resolve("./src/components"),
+      "@pages": path.resolve("./src/pages"),
+      "@features": path.resolve("./src/features"),
+      "@styles": path.resolve("./src/styles"),
+      "@utils": path.resolve("./src/utils"),
+      "@hooks": path.resolve("./src/hooks"),
+    },
+  },
+});
